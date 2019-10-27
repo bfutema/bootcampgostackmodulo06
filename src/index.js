c5db7313edf893b03353b7d9fe35ebeb088dfe7a
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-10-26 21:45:36 -0300

    feature: React Navigation

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 0,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-    </View>
-  );
+  return <Routes />;
 }
